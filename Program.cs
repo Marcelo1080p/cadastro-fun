ee6990c0b8d8aa58d956549b0642c9bdcfb4df0e
Marcelo1080p <juniooor.2015@gmail.com>
2024-04-20 13:18:24 -0300

    refatoracao da entrada em Program

--- Program.cs
+++ Program.cs
@@ -6,26 +6,33 @@ while (fimPrograma == 0)
 {
     Console.WriteLine("Opções disponiveis abaixo\n[1] Ver funcioários\n[2] Cadastrar funcionário\n[3] Sair do programa\nInforme a opção desejada: ");
     string entrada = Console.ReadLine();
-    if (entrada == "1")
+    switch (entrada)
     {
-        CadastrarFunc.VerFuncionario();
-    }
-    else if (entrada == "2")
-    {
-        int matricula = 1;
-        Console.Write("Informe o nome do colaborador: ");
-        string nome = Console.ReadLine();
-        Console.Write("Informe a idade do colaborador: ");
-        string idadeString = Console.ReadLine();
-        int idade = Convert.ToInt32(idadeString);
-        Console.Write("Informe o salário inicial: ");
-        string salarioString = Console.ReadLine();
-        decimal salario = Convert.ToDecimal(salarioString);
-        CadastrarFunc.AddFuncionario(matricula, nome, idade, salario);
-    }
-    else if (entrada == "3")
-    {
-        Console.Write("Obrigado por utilizar nosso programa!");
-        fimPrograma = 1;
+        case "1":
+            CadastrarFunc.VerFuncionario();
+            break;
+
+        case "2":
+
+            int matricula = 1;
+            Console.Write("Informe o nome do colaborador: ");
+            string nome = Console.ReadLine();
+            Console.Write("Informe a idade do colaborador: ");
+            string idadeString = Console.ReadLine();
+            int idade = Convert.ToInt32(idadeString);
+            Console.Write("Informe o salário inicial: ");
+            string salarioString = Console.ReadLine();
+            decimal salario = Convert.ToDecimal(salarioString);
+            CadastrarFunc.AddFuncionario(matricula, nome, idade, salario);
+            break;
+
+        case "3":
+
+            Console.Write("Obrigado por utilizar nosso programa!");
+            fimPrograma = 1;
+            break;
+        default:
+            Console.WriteLine("Entrada Invalida");
+            break;
     }
 }
